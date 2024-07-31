7df7a5fcfa4ad34107b8703d39313daa7fce0ecb
lela-akiashvili <l.akiashvili.doit@itstep.ge>
2024-07-31 18:28:41 +0400

    feat: updated initial project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/sign-in/sign-in.component').then(
+        (m) => m.SignInComponent
+      ),
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //   { path: '**', component: NotFoundComponent },
+];
